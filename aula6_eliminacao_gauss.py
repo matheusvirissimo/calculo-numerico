e8f439a9cb7caf471e62997b4b5502051382fff1
matheusvirissimo <matheus.virissimo@unesp.br>
2025-04-17 14:10:48 -0300

    bug: problema no range das iterações

--- aula6_eliminacao_gauss.py
+++ aula6_eliminacao_gauss.py
@@ -5,6 +5,9 @@ def eliminicao_gauss(A, b):
         for i in range(k+1,n): # percorre as linhas abaixo da primeira para zerar a coluna k 
             b[i] = b[i] - (A[i][k]/A[k][k]) * b[k] #atualiza o vetor b (possível divisão por 0 aqui)
             
+
+            #quando k+1, os valores de b são atualizados corretamente, mas não de A
+            #quando k, os valores de A são atualizados corretamente, mas não de b
             for j in range(k+1,n): # percorre a coluna a partir da que estamos atualmente
                 A[i][j] = A[i][j] - (A[i][k]/A[k][k] * A[k][j]) # atualiza os coeficientes de A (possível divisão por 0)
         
@@ -23,21 +26,22 @@ def determinante(A):
     return det
 
 """
-    | 2  1  -1 |
-A = | -3  -1  2 |
-    | -2  1  2 |
+    | 2  -3  1 |
+A = | 4  -6  -1 |
+    | 1  2  1 |
 """
 
 if __name__ == "__main__":
 
-    matriz_a = [[2,1,-1], 
-                [-3,-1,2], 
-                [-2,1,2]]
+    matriz_a = [[2,3,1], 
+                [4,7,5], 
+                [6,18,11]]
 
-    b = [8, -11, -3]
+    b = [1, 3, 6]
 
     matriz_escalonada, termos_independentes = eliminicao_gauss(matriz_a, b)
 
     det = determinante(matriz_escalonada)
 
-    print(matriz_escalonada, termos_independentes, det)
+    print(matriz_escalonada)
+    print(termos_independentes)
